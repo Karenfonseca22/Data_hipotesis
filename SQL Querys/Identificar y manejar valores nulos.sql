c7540dc99234a566bded97fcead047e826e12e7e
Karen Fonseca <149629605+Karenfonseca22@users.noreply.github.com>
2024-07-11 18:50:01 -0500

    Update and rename Identificar y manejar valores nulos to Identificar y manejar valores nulos.sql

--- SQL Querys/Identificar y manejar valores nulos.sql
+++ SQL Querys/Identificar y manejar valores nulos.sql
@@ -12,3 +12,13 @@ WHERE
   OR in_deezer_charts IS NULL
   OR in_shazam_charts IS NULL;
 ```
+SELECT
+  COUNT(*) AS cantidad_nulos
+FROM
+  `proyecto-hipotesis-lab1.dataset.track-in-competition`
+WHERE
+  track_id IS NULL
+  OR in_apple_playlists IS NULL
+  OR in_apple_charts IS NULL
+  OR in_deezer_charts IS NULL
+  OR in_shazam_charts IS NULL 
