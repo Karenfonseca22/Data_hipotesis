219114e12b73067a82f48837d291757a94c30615
Karen Fonseca <149629605+Karenfonseca22@users.noreply.github.com>
2024-07-12 14:50:17 -0500

    Create Datos discrepantes.sql

---
+++ SQL Querys/Datos discrepantes.sql
@@ -0,0 +1,17 @@
+## Datos discrepantes
+--- Formula para pasar de STRING A INTEGER
+SELECT *,
+IF
+  (REGEXP_CONTAINS(streams, r'^[0-9]+$'), CAST(streams AS INT64), NULL) AS stream_limpio
+FROM
+  `proyecto-hipotesis-lab1.dataset.track_in_spotify`
+WHERE
+  streams NOT LIKE '%[^0-9]%'
+
+-- Formula para sacar el AVG, MAX y MIN
+
+  SELECT
+MAX(stream_limpio) AS maximo,
+MIN(stream_limpio) AS minimo,
+AVG (stream_limpio) AS promedio,
+FROM `proyecto-hipotesis-lab1.dataset.cast_integer_stream`
